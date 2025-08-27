aea0f7a25dad93a4b6d7e75028c7a363cf48675a
BeniaminKohnke <74410312+BeniaminKohnke@users.noreply.github.com>
2025-08-27 13:14:04 +0200

    Loops over shorts nodes

--- contentScripts.js
+++ contentScripts.js
@@ -1,11 +1,18 @@
+const xpath = '//ytd-rich-section-renderer[.//span[@id="title" and text()="Shorts"]] '
+    + '| //ytd-mini-guide-entry-renderer[.//span[@class="title style-scope ytd-mini-guide-entry-renderer" and text()="Shorts"]] '
+    + '| //ytd-guide-entry-renderer[.//yt-formatted-string[text()="Shorts"]]' 
+    + '| //ytd-reel-shelf-renderer'
+    + '| //div[@id="shorts-container"]';
+
 setInterval(() => {
-    document.evaluate('//ytd-rich-section-renderer[.//span[@id="title" and text()="Shorts"]]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue?.remove();
-    document.evaluate('//ytd-mini-guide-entry-renderer[.//span[@class="title style-scope ytd-mini-guide-entry-renderer" and text()="Shorts"]]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue?.remove();
-    document.evaluate('//ytd-guide-entry-renderer[.//yt-formatted-string[text()="Shorts"]]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue?.remove();
-    document.evaluate('//ytd-reel-shelf-renderer', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue?.remove();
-    document.evaluate('//div[@id="shorts-container"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue?.remove();
-    
-    if (window.location.href.startsWith('https://www.youtube.com/shorts/')) {
+    if ((window.location.href.includes('youtube') || window.location.href.includes('youtu.be')) && window.location.href.includes('/shorts/')) {
         window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+        return;
+    }
+
+    let element = null;
+    while (element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue) {
+        console.log('removed: ' + element.innerHTML);
+        element.remove();
     }
 }, 1000);
